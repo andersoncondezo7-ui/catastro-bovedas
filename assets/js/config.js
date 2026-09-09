1b3c8915fcf53fbb4c607bcbaeec338fe1a1ef10
andersoncondezo7-ui <andersoncondezo7@gmail.com>
2026-09-08 22:34:33 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = Object.freeze({
   appName: "Inspección de bóvedas",
-  schemaVersion: "5.2.0",
+  schemaVersion: "5.3.0",
   userSessionKey: "catastro-bovedas:inspector",
   zoneSessionKey: "catastro-bovedas:zone",
   zones: Object.freeze({
